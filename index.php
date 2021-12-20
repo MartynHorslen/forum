4a3e2af69cd803af17a32423468cbc2943e4f6db
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2021-12-20 16:38:26 +0000

    Rewrite the forum overview and convert from MySQLi to PDO

--- index.php
+++ index.php
@@ -2,13 +2,17 @@
     include 'head.php';
     include 'header.php';
 
+    //Forum Bootstrap Body
+    echo '<div class="row no-gutters align-items-center"><!-- open row -->
+    <div class="col-sm-10 col-md-8 offset-sm-1 offset-md-2"> ';
+
     //Forum Navigation
 
 
     //Forum page set by url view.
     if (!isset($_GET['view'])) {
-        //include 'overview.php';
-        echo 'Forum Overview';
+        include 'overview.php';
+        //echo 'Forum Overview';
     } else if ($_GET['view'] == "cat") {
         //include 'category.php';
         echo 'Forum Category view';
@@ -20,11 +24,13 @@
         echo 'Forum New Posts view';
     }  else if ($_GET['view'] == "create"){
         //include 'create.php';
-        echo 'Create a new topic.'
+        echo 'Create a new topic.';
     } else {
         echo "Error";
     }
 
+    //Closing Forum Bootstrap Body
+    echo '</div></div>';
 
     include 'footer.php';
 ?>
---
+++ overview.php
@@ -0,0 +1,95 @@
+<div class="card my-2 p-2">
+<?php 
+    //Get a list of forum categories
+    $sql = "SELECT cat_id, cat_name, cat_description FROM categories ORDER BY cat_order ASC";
+    $categories = $dbh->prepare($sql);
+    $categories->execute();
+    $categories = $categories->fetchAll();
+    
+    //Catch empty category table and query error
+    if(!$categories)
+    {
+        echo 'The categories could not be displayed, please try again later.';
+    }
+    else
+    {
+        if($categories->rowCount === 0)
+        {
+            echo 'No categories defined yet.';
+        }
+        else
+        {     
+            // Loop through each category counting number of topics and getting the category ID
+            foreach($categories as $catRow) {
+                $cat_id = $catRow['cat_id'];
+                $sql = "SELECT * FROM topics WHERE topic_cat = ' $cat_id '";
+                $topics = $dbh->prepare($sql);
+                $topics->execute();
+                $topics = $topics->fetchAll();
+                $numOfTopics = count($topics);
+
+                //Loop through the topics counting the number of posts and topic ID
+                $numOfPosts = 0;
+                foreach($topics as $topicRow) {
+                    $topic_id = $topicRow['topic_id'];
+                    $sql = "SELECT * FROM posts WHERE post_topic = ' $topic_id '";
+                    $posts = $dbh->prepare($sql);;
+                    $posts->execute();
+                    $posts = $posts->fetchAll();
+                    $numOfPosts += count($posts);
+                }
+
+                //Select the latest topic from this category and join with user details ready to be displayed on the forum overview
+                $sql = "SELECT topics.topic_subject, topics.topic_by, topics.topic_date, users.user_id, users.user_name FROM topics LEFT JOIN users ON topics.topic_by = users.user_id WHERE topics.topic_id = $topic_id Order By topic_date LIMIT 1";
+                $lastTopic = $dbh->prepare($sql);
+                $lastTopic->execute();
+                $lastTopic = $lastTopic->fetch();
+
+                //Create the forum overview structure and include category names, latest topic, date, username and also topic and reply counts with url links to relevant user and post pages.
+                echo '<div class="card mb-2">
+                    <div class="card-header px-3">
+                        <h5 class="mb-0"><a href="index.php?view=cat&cat=' . $catRow['cat_id'] . '">' . $catRow['cat_name'] . '</a></h5>
+                    </div>
+                    <div class="row">
+                        <div class="col-9 col-sm-8">
+                            <div class="card-body py-0 px-2">
+                                ' . $lastTopic['cat_description'] . '
+                            </div>
+                            <div class="card-body py-0 px-2">
+                                Latest Topic: <a href="index.php?view=topic&topic=' . $topic_id . '">' . $lastTopic['topic_subject'] . '</a> posted by <a href="#">' . $lastTopic['user_name'] . '</a> 
+                            </div>
+                            <div class="card-body py-0 px-2">
+                                Posted at ' . date("F j, Y, g:i a", strtotime($lastTopic['topic_date'])) . '
+                            </div>
+                        </div>
+                        <div class="col-3 col-sm-4">
+                            <div class="row">
+                                <div class="col col-sm-6 py-2">
+                                    <div class="card-body py-0 px-2 text-center">
+                                        ' . $numOfTopics . '
+                                    </div>
+                                    <div class="card-body py-0 px-2 text-center">
+                                        Topics
+                                    </div>  
+                                </div>
+                                <div class="col col-sm-6 py-2">              
+                                    <div class="card-body py-0 px-2 text-center">
+                                        ' . $numOfPosts . '
+                                    </div>
+                                    <div class="card-body py-0 px-2 text-center">
+                                        Posts
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <div class="row">
+                
+                
+                </div>
+            </div>';
+            }
+        }
+    }
+?>
+</div>

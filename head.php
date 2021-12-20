2bbb1d3459c28287a0b51d378bf0a312d6929fb7
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2021-12-20 14:29:28 +0000

    Include DB connection in head.php and update page title

--- head.php
+++ head.php
@@ -1,13 +1,16 @@
 <?php
     //Start session before anything else runs.
     session_start();
+
+    //Open DB connection as most pages are going to interact with the DB anyway.
+    include 'connect.php';
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Bootstrap Forum made in PHP & MySQL</title>
         <!--<meta name="description" content="">-->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Bootstrap CSS -->

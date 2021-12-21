ef47af7d4960c5a657c7b1976d740aa9db45a87e
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2021-12-21 11:21:26 +0000

    Update home button href

--- navigation.php
+++ navigation.php
@@ -1,7 +1,7 @@
 <div class="card mb-2 px-3 py-2">
     <div class="row">
         <div class="col-12 col-sm-6">
-            <a class="btn btn-primary btn-sm m-1" href="/forum/">Home</a>
+            <a class="btn btn-primary btn-sm m-1" href="index.php">Home</a>
             <a class="btn btn-primary btn-sm m-1" href="index.php?view=new">New Posts</a>
 
             <?php 

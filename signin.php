939cd78c0155d6b1c6dc330d378d470011300647
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2021-12-27 17:50:32 +0000

    Add card styling to login form

--- signin.php
+++ signin.php
@@ -29,15 +29,21 @@
                     //Else display errors and increment a login counter (Basic $session counter or advanced IP, Username, datetime tracked in DB).
         } else {
             //Else display the form and registration link.
-            echo '<form class="" method="post" action="">
-            <div class="form-group">
-                <input type="username" class="form-control text-center" id="user_name" name="user_name" placeholder="Username">
-            </div>
-            <div class="form-group">
-                <input type="password" class="form-control text-center" id="user_pass" name="user_pass" placeholder="Password">
-            </div>
-            <div class="form-group"><button type="submit" class="btn btn-secondary btn-md w-100" name="sign-in-button">Sign In</button></div>
-        </form>';
+            echo '
+            <div class="card border-secondary mb-3 m-auto" style="max-width: 18rem;">
+                <div class="card-header">Login:</div>
+                <div class="card-body text-secondary">
+                    <form class="" method="post" action="">
+                        <div class="form-group">
+                            <input type="username" class="form-control text-center" id="user_name" name="user_name" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control text-center" id="user_pass" name="user_pass" placeholder="Password">
+                        </div>
+                        <div class="form-group"><button type="submit" class="btn btn-secondary btn-md w-100" name="sign-in-button">Sign In</button></div>
+                    </form>
+                </div>
+            </div>';
         } 
     }
 ?>

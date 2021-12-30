e78b7b1aadf678af13630a607bcf276edf9c3c8c
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2021-12-30 16:50:39 +0000

    Correct unset session variables

--- signout.php
+++ signout.php
@@ -1,7 +1,8 @@
 <?php 
     session_start();
-    unset($_SESSION['user']);
-    unset($_SESSION['pass']);
+    unset($_SESSION['user_id']);
+    unset($_SESSION['user_name']);
+    unset($_SESSION['user_level']);
     session_destroy();
     header('Location:index.php'); 
 ?>

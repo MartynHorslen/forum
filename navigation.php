336e605ee375883c592fc99b0c99ac8b3548d7a9
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2021-12-31 15:57:30 +0000

    Add registration links to forum navigation

--- navigation.php
+++ navigation.php
@@ -26,10 +26,13 @@
                     echo '<a href="index.php?view=signin">Sign in</a> or <a href="#">create an account</a>.';
                 } else if ($_GET['view'] === 'topic'){
                     //set 'previous' to topic view and topic number
-                    echo '<a href="index.php?view=signin&previous=topic&topic=' . $_GET['topic'] . '">Sign in</a> or <a href="#">create an account</a>.';
+                    echo '<a href="index.php?view=signin&previous=topic&topic=' . $_GET['topic'] . '">Sign in</a> or <a href="index.php?view=register&previous=topic&topic=' . $_GET['topic'] . '"create an account</a>.';
+                } else if ($_GET['view'] === 'cat'){
+                    //set 'previous' to topic view and topic number
+                    echo '<a href="index.php?view=signin&previous=cat&cat=' . $_GET['cat'] . '">Sign in</a> or <a href="index.php?view=register&previous=cat&cat=' . $_GET['cat'] . '"create an account</a>.';
                 } else {
                     //set 'previous' to current view
-                    echo '<a href="index.php?view=signin&previous=' . $_GET['view'] . '">Sign in</a> or <a href="#">create an account</a>.';
+                    echo '<a href="index.php?view=signin&previous=' . $_GET['view'] . '">Sign in</a> or <a href="index.php?view=register&previous=' . $_GET['view'] . '"create an account</a>.';
                 }
             }
             ?>

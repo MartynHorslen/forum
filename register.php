4d16f9ea42e4e5961e248916d4d1317d410e7295
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2022-01-01 22:21:52 +0000

    Add logic to check if form has been submitted

--- register.php
+++ register.php
@@ -16,27 +16,35 @@
             header('Location:index.php');
         }
     } else {
-        echo '<div class="card border-secondary mb-3 m-auto" style="max-width: 28rem;">
-            <div class="card-header">Registration:</div>
-            <div class="card-body text-secondary">
-                <form class="" method="post" action="">
-                    <div class="form-group">
-                        <input type="username" class="form-control text-center" id="user_name" name="user_name" placeholder="Username">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control text-center" id="user_pass" name="user_pass" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control text-center" id="user_pass_check" name="user_pass_check" placeholder="Confirm Password">
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control text-center" id="user_email" name="user_email" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" name="register-button"  class="btn btn-secondary btn-md w-100">Register!</button>
-                    </div>
-                </form>
-            </div>
-        </div>';
+        //Check whether the login form has been submitted
+        if($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            //process form
+            echo 'form processing...';
+        } else {
+            //Display form
+            echo '<div class="card border-secondary mb-3 m-auto" style="max-width: 28rem;">
+                <div class="card-header">Registration:</div>
+                <div class="card-body text-secondary">
+                    <form class="" method="post" action="">
+                        <div class="form-group">
+                            <input type="username" class="form-control text-center" id="user_name" name="user_name" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control text-center" id="user_pass" name="user_pass" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control text-center" id="user_pass_check" name="user_pass_check" placeholder="Confirm Password">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control text-center" id="user_email" name="user_email" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="register-button"  class="btn btn-secondary btn-md w-100">Register!</button>
+                        </div>
+                    </form>
+                </div>
+            </div>';
+        }
     }
 ?>

278e5dbb9b14158ce6101518239aca35c03e5eee
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2022-01-01 22:12:53 +0000

    Add registration form and remove forum nav on register page

--- register.php
+++ register.php
@@ -1,3 +1,24 @@
 <?php
-    echo 'This is the registration page.';
+    echo '<div class="card border-secondary mb-3 m-auto" style="max-width: 28rem;">
+        <div class="card-header">Registration:</div>
+        <div class="card-body text-secondary">
+            <form class="" method="post" action="">
+                <div class="form-group">
+                    <input type="username" class="form-control text-center" id="user_name" name="user_name" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control text-center" id="user_pass" name="user_pass" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control text-center" id="user_pass_check" name="user_pass_check" placeholder="Confirm Password">
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control text-center" id="user_email" name="user_email" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <button type="submit" name="register-button"  class="btn btn-secondary btn-md w-100">Register!</button>
+                </div>
+            </form>
+        </div>
+    </div>';
 ?>

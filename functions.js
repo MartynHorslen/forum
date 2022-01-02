2641f6202c60637d39f703d2bbe3d3e3a6c097ef
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2022-01-02 17:16:01 +0000

    add functions.js file for reply and create topic ajax functions

---
+++ functions.js
@@ -0,0 +1,59 @@
+function reply() {
+    var replyContent = $("#reply-content").val();
+    var userId = $("#user-id").val();
+    var topicId = $("#topic-id").val();
+
+    if (replyContent === "" )
+    {
+        alert("Please fill in the reply field");
+    } else {
+        $.ajax({
+            async: true,
+            type: 'POST',
+            //cache: false,
+            url: 'reply.php',
+            data: {
+                replyContent:replyContent,
+                userId:userId,
+                topicId:topicId
+            },
+            success: function(response) {
+                console.log("Reply posted successfully");
+            },
+            error: function(response) {
+                alert('Error: ', response);
+            }
+        });
+    }
+}
+
+function createTopic() {
+    var topicSubject = $("#topic_subject").val();
+    var topicCat = $("#topic_cat").val();
+    var postContent = $("#post_content").val();
+    var userId = $("#user-id").val();
+
+    if (topicSubject === "" || postContent === "" )
+    {
+        alert("Please fill in the fields");
+    } else {
+        $.ajax({
+            async: true,
+            type: 'POST',
+            //cache: false,
+            url: 'create_topic.php',
+            data: {
+                userId:userId,
+                topicSubject:topicSubject,
+                topicCat:topicCat,
+                postContent:postContent
+            },
+            success: function(response) {
+                window.location.assign(response);
+            },
+            error: function(response) {
+                alert('Error: ', response);
+            }
+        });
+    }
+}

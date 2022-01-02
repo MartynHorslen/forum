068fb73aa45c6c5bb936abd93891120fbb794d4f
Martyn Horslen <2880060+MartynHorslen@users.noreply.github.com>
2022-01-02 00:26:44 +0000

    Allow form with errors to remember user input when returning the form

--- register.php
+++ register.php
@@ -72,16 +72,16 @@
                         echo '<div class="card-body text-secondary">
                             <form class="" method="post" action="">
                                 <div class="form-group">
-                                    <input type="username" class="form-control text-center" id="user_name" name="user_name" placeholder="Username">
+                                    <input type="username" class="form-control text-center" id="user_name" name="user_name" value="' . $_POST['user_name'] . '" placeholder="' . $_POST['user_name'] . '">
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" class="form-control text-center" id="user_pass" name="user_pass" placeholder="Password">
+                                    <input type="password" class="form-control text-center" id="user_pass" name="user_pass" value="' . $_POST['user_pass'] . '" placeholder="Password">
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" class="form-control text-center" id="user_pass_check" name="user_pass_check" placeholder="Confirm Password">
+                                    <input type="password" class="form-control text-center" id="user_pass_check" name="user_pass_check" value="' . $_POST['user_pass_check'] . '" placeholder="Confirm Password">
                                 </div>
                                 <div class="form-group">
-                                    <input type="email" class="form-control text-center" id="user_email" name="user_email" placeholder="Email">
+                                    <input type="email" class="form-control text-center" id="user_email" name="user_email" value="' . $_POST['user_email'] . '" placeholder="' . $_POST['user_email'] . '">
                                 </div>
                                 <div class="form-group">
                                     <button type="submit" name="register-button"  class="btn btn-secondary btn-md w-100">Register!</button>
